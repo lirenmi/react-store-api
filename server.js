21564b2faa2cb6e681a2b061b685c7e289dbb05c
lirenmi <lirenmi@163.com>
2019-11-03 12:05:50 +0800

    用户注册

--- server.js
+++ server.js
@@ -46,6 +46,50 @@ server.post('/auth/login', (req, res) => {
   }
 });
 
+// Register New User
+server.post('/auth/register', (req, res) => {
+  const { email, password, nickname, type } = req.body;
+
+  // ----- 1 step
+  if (isAuthenticated({ email, password })) {
+    const status = 401;
+    const message = 'Email and Password already exist';
+    return res.status(status).json({ status, message });
+  }
+
+  // ----- 2 step
+  fs.readFile(path.join(__dirname, 'users.json'), (err, _data) => {
+    if (err) {
+      const status = 401;
+      const message = err;
+      return res.status(status).json({ status, message });
+    }
+    // Get current users data
+    const data = JSON.parse(_data.toString());
+    // Get the id of last user
+    const last_item_id = data.users[data.users.length - 1].id;
+    //Add new user
+    data.users.push({ id: last_item_id + 1, email, password, nickname, type }); //add some data
+    fs.writeFile(
+      path.join(__dirname, 'users.json'),
+      JSON.stringify(data),
+      (err, result) => {
+        // WRITE
+        if (err) {
+          const status = 401;
+          const message = err;
+          res.status(status).json({ status, message });
+          return;
+        }
+      }
+    );
+  });
+
+  // Create token for new user
+  const jwToken = createToken({ nickname, type, email });
+  res.status(200).json(jwToken);
+});
+
 server.use(router);
 server.listen(3003, () => {
   console.log('JSON Server is running');
